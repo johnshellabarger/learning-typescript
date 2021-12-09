27281ec0911c12eb10555471d02f827a3c6ce3b1
John Shellabarger <johnwilliamshellabarger@gmail.com>
2021-12-08 21:12:53 -0700

    basics

--- helloWorld.ts
+++ helloWorld.ts
@@ -1,7 +1,62 @@
-function hello(string : String){
+function hello(string : string){
   console.log("Hello " + string)
 }
 
-hello("Johnny Shellabarger!")
+interface Stark {
+  name: string,
+  age?: number
+}
 
+function printName(stark : Stark){
+  console.log(stark.name)
+}
+
+printName({name: "Eddard"})
+// printName({label: "Eddard"}) is undefined so the interface throws an error.... ? means it is optional in the object
+
+
+// Properties cannot be declared optionally, however they can implement interfaces with optional properties
+class Dog {
+  name: string = "Kaya";
+  static bone: string = "Milk bone"
+  saying: string;
+
+  constructor(){
+    this.saying = "Barf"
+  }
+
+  hello(person : string){
+    console.log("Hello, " + person)
+  }
+}
+
+var ned = new Dog()
+ned.saying = "Woof Woof"
+
+ned.hello("Robert")
+
+
+class Person {
+  name: string;
+  constructor(name:string){
+    this.name = name
+  }
+
+  dance(){
+    console.log(this.name + " is dancing")
+  }
+}
+
+class AwesomePerson extends Person {
+  dance(){
+    super.dance()  
+    console.log("SO AWESOME!")
+  }
+}
+
+let johnny = new Person("Johnny")
+johnny.dance()
+
+let robb = new AwesomePerson("Robb")
+robb.dance()
 
--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,62 @@
-// function hello(string : String){
-//   console.log("Hello " + string)
-// }
-// hello("Johnny!")
-var isWinter = false;
-var name = 'Bran';
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+function hello(string) {
+    console.log("Hello " + string);
+}
+function printName(stark) {
+    console.log(stark.name);
+}
+printName({ name: "Eddard" });
+// printName({label: "Eddard"}) is undefined so the interface throws an error.... ? means it is optional in the object
+// Properties cannot be declared optionally, however they can implement interfaces with optional properties
+var Dog = /** @class */ (function () {
+    function Dog() {
+        this.name = "Kaya";
+        this.saying = "Barf";
+    }
+    Dog.prototype.hello = function (person) {
+        console.log("Hello, " + person);
+    };
+    Dog.bone = "Milk bone";
+    return Dog;
+}());
+var ned = new Dog();
+ned.saying = "Woof Woof";
+ned.hello("Robert");
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    Person.prototype.dance = function () {
+        console.log(this.name + " is dancing");
+    };
+    return Person;
+}());
+var AwesomePerson = /** @class */ (function (_super) {
+    __extends(AwesomePerson, _super);
+    function AwesomePerson() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    AwesomePerson.prototype.dance = function () {
+        _super.prototype.dance.call(this);
+        console.log("SO AWESOME!");
+    };
+    return AwesomePerson;
+}(Person));
+var johnny = new Person("Johnny");
+johnny.dance();
+var robb = new AwesomePerson("Robb");
+robb.dance();
